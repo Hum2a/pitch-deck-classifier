e5ff3816c43ede80beabc91d04c9463966351cad
Humza Butt <Humzab1711@hotmail.com>
2024-11-18 13:14:25 +0000

    config adjusted

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const apiUrl = process.env.NODE_ENV === 'production' 
-    ? 'https://your-render-backend-url.onrender.com' 
-    : 'http://localhost:5000';
+    ? 'https://pitch-deck-classifier-1.onrender.com' 
+    : 'http://localhost:5000'
 
 export default apiUrl;
