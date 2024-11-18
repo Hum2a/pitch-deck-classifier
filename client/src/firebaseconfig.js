983119a97c04148cf9127067366e3138d3635dbf
Humza Butt <Humzab1711@hotmail.com>
2024-11-18 13:18:23 +0000

    firebase config update

--- client/src/firebaseconfig.js
+++ client/src/firebaseconfig.js
@@ -4,15 +4,26 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCLk98R3PwHZYjTY-Ed3xFeGmy4tnZnDzU",
+//   authDomain: "pitchdeckclassifier.firebaseapp.com",
+//   projectId: "pitchdeckclassifier",
+//   storageBucket: "pitchdeckclassifier.firebasestorage.app", // Use correct bucket format
+//   messagingSenderId: "823771885885",
+//   appId: "1:823771885885:web:dae23c3cccfc69d8f51554",
+//   measurementId: "G-FLLSFK7H1Z"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCLk98R3PwHZYjTY-Ed3xFeGmy4tnZnDzU",
-  authDomain: "pitchdeckclassifier.firebaseapp.com",
-  projectId: "pitchdeckclassifier",
-  storageBucket: "pitchdeckclassifier.firebasestorage.app", // Use correct bucket format
-  messagingSenderId: "823771885885",
-  appId: "1:823771885885:web:dae23c3cccfc69d8f51554",
-  measurementId: "G-FLLSFK7H1Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
